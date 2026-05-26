bda3a24c32e267a8a7517b42ea8b79d763b694bc
RanikyaSwathi <swathi.r@softsuave.org>
2026-05-26 19:03:02 +0530

    initial commit

---
+++ src/test/java/com/gitpractisce/GitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpractisce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
